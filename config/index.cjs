d3e7779628d1f06d77ee797623c5189b21f0607a
Leyusf <53003567+Leyusf@users.noreply.github.com>
2022-12-31 15:56:54 +0000

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -34,7 +34,7 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '宝贝',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'oXWJk6JPMDfQQi6PTFA9nM7D4uD4',
+      id: 'oXWJk6JpAGufR27kWiKpW4uO2VRI',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: 'vFxNyfPYKQqGOPCXj_pXvE8P9o5kRR20Zei71SvJgG4',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
